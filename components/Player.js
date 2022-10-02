cd8a32165401bdcd00f2c85947c89c851304f28b
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-10-02 00:05:37 -0700

    Debounce the volume adjustment for the range input

--- components/Player.js
+++ components/Player.js
@@ -1,13 +1,15 @@
 import {
   ArrowsRightLeftIcon,
   ArrowUturnLeftIcon,
-  PauseIcon
+  SpeakerWaveIcon as SpeakerWaveOutlineIcon
 } from '@heroicons/react/24/outline';
-import { BackwardIcon, ForwardIcon, PlayCircleIcon } from '@heroicons/react/24/solid';
+import { BackwardIcon, ForwardIcon, PauseCircleIcon, PlayCircleIcon, SpeakerWaveIcon } from '@heroicons/react/24/solid';
+
+import { debounce } from 'lodash';
 
 
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRecoilState } from 'recoil';
 import { currentTrackIdStateAtom, isPlayingStateAtom } from "../atoms/song.atom";
 import useSongInfo from "../hooks/useSongInfo.hook";
@@ -55,6 +57,18 @@ const Player = () => {
     }
   }, [currentTrackId, spotifyApi, session]);
 
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debouncedAdjustVolume(volume);
+    }
+  }, [volume]);
+
+  const debouncedAdjustVolume = useCallback(
+    debounce(() => {
+      spotifyApi.setVolume(volume).catch(error => {});
+    }, 500, [])
+  );
+
   return (
     <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
       {/* Left */}
@@ -75,7 +89,7 @@ const Player = () => {
 
         {
           isPlaying ? (
-            <PauseIcon onClick={handleTogglePlayer} className='button w-10 h-10' />
+            <PauseCircleIcon onClick={handleTogglePlayer} className='button w-10 h-10' />
           ) : (
             <PlayCircleIcon onClick={handleTogglePlayer} className='button w-10 h-10' />
           )
@@ -83,6 +97,13 @@ const Player = () => {
 
         <ForwardIcon onClick={() => spotifyApi.skipToNext()} className='button' />
         <ArrowUturnLeftIcon className='button' />
+      </div>
+
+      {/* Right */}
+      <div className='flex items-center space-x-3 md:space-x-4 justify-end pr-5'>
+        <SpeakerWaveOutlineIcon onClick={() => volume > 0 && setVolume(volume - 10)} className='button' />
+        <input type='range' onChange={e => setVolume(Number(e.target.value))} className='w-14 md:w-28' value={volume} min={0} max={100} />
+        <SpeakerWaveIcon onClick={() => volume < 100 && setVolume(volume + 10)} className='button' />
       </div>
     </div>
   )
