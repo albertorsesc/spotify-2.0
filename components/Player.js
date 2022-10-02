6c494b40c7a9582b6b042d93de35c18aa30c8953
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-10-01 23:49:05 -0700

    Play/Pause a Song functionality added

--- components/Player.js
+++ components/Player.js
@@ -1,3 +1,11 @@
+import {
+  ArrowsRightLeftIcon,
+  ArrowUturnLeftIcon,
+  PauseIcon
+} from '@heroicons/react/24/outline';
+import { BackwardIcon, ForwardIcon, PlayCircleIcon } from '@heroicons/react/24/solid';
+
+
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRecoilState } from 'recoil';
@@ -26,7 +34,19 @@ const Player = () => {
         })
       })
     }
-  }
+  };
+
+  const handleTogglePlayer = () => {
+    spotifyApi.getMyCurrentPlaybackState().then(data => {
+      if (data.body.is_playing) {
+        spotifyApi.pause();
+        setIsPlaying(false);
+      } else {
+        spotifyApi.play();
+        setIsPlaying(true);
+      }
+    })
+  };
 
   useEffect(() => {
     if (spotifyApi.getAccessToken() && !currentTrackId) {
@@ -36,11 +56,33 @@ const Player = () => {
   }, [currentTrackId, spotifyApi, session]);
 
   return (
-    <div>
+    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
       {/* Left */}
-      <div>
+      <div className="flex items-center space-x-4">
         <img src={songInfo?.album?.images?.[0]?.url}
           className='hidden md:inline h-10 w-10' />
+
+          <div>
+            <h3>{songInfo?.name}</h3>
+            <p>{songInfo?.artists?.[0]?.name}</p>
+          </div>
+      </div>
+
+      {/* Center */}
+      <div className='flex items-center justify-evenly'>
+        <ArrowsRightLeftIcon className='button' />
+        <BackwardIcon onClick={() => spotifyApi.skipToPrevious()} className='button' />
+
+        {
+          isPlaying ? (
+            <PauseIcon onClick={handleTogglePlayer} className='button w-10 h-10' />
+          ) : (
+            <PlayCircleIcon onClick={handleTogglePlayer} className='button w-10 h-10' />
+          )
+        }
+
+        <ForwardIcon onClick={() => spotifyApi.skipToNext()} className='button' />
+        <ArrowUturnLeftIcon className='button' />
       </div>
     </div>
   )
